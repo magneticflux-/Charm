cbb762ccb5525cdfb89ca8cf7fbe1725bd6e3607
Coranthes <josa23oreo@gmail.com>
2021-04-04 17:11:47 -0400

    Gold lantern stuff

--- src/main/java/svenhjol/charm/block/GoldLanternBlock.java
+++ src/main/java/svenhjol/charm/block/GoldLanternBlock.java
@@ -4,8 +4,21 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
 import svenhjol.charm.base.CharmModule;
 
+import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import svenhjol.charm.base.CharmModule;
+
 public class GoldLanternBlock extends BaseLanternBlock {
+    private static final VoxelShape STANDING_SHAPE = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 11.0D, 11.0D);
+    private static final VoxelShape HANGING_SHAPE = Block.createCuboidShape(5.0D, 4.0D, 5.0D, 11.0D, 15.0D, 11.0D);
+
     public GoldLanternBlock(CharmModule module, String name) {
         super(module, name, AbstractBlock.Settings.copy(Blocks.LANTERN));
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return state.get(HANGING) ? HANGING_SHAPE : STANDING_SHAPE;
     }
 }
