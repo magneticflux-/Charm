7fa44df9c9dac82c5bd7ff186d622714a475e681
Sven Hjól <sven.hjol@protonmail.com>
2021-04-15 02:05:32 +0000

    Aggressive goats for no reason

--- src/main/java/svenhjol/charm/mixin/accessor/GoatBrainAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/GoatBrainAccessor.java
@@ -2,6 +2,7 @@ package svenhjol.charm.mixin.accessor;
 
 import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.passive.GoatBrain;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -9,6 +10,11 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface GoatBrainAccessor {
     @Accessor("field_33500")
     static void setTargetPredicate(TargetPredicate targetPredicate) {
+        throw new IllegalStateException();
+    }
+
+    @Accessor("field_33499")
+    static void setRamTicks(UniformIntProvider uniformIntProvider) {
         throw new IllegalStateException();
     }
 }
--- src/main/java/svenhjol/charm/module/GoatImprovements.java
+++ src/main/java/svenhjol/charm/module/GoatImprovements.java
@@ -2,16 +2,24 @@ package svenhjol.charm.module;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
 import svenhjol.charm.mixin.accessor.GoatBrainAccessor;
 
-@Module(mod = Charm.MOD_ID)
+@Module(mod = Charm.MOD_ID, description = "Goats are cool.")
 public class GoatImprovements extends CharmModule {
+    @Config(name = "Overly aggressive goats", description = "If true, goats will ram nearby entities much more frequently.")
+    public static boolean aggressive = false;
+
     @Override
     public void init() {
         GoatBrainAccessor.setTargetPredicate((new TargetPredicate()).setPredicate((livingEntity)
             -> !livingEntity.getType().equals(EntityType.GOAT) && !livingEntity.getType().equals(EntityType.ARMOR_STAND)));
+
+        if (aggressive)
+            GoatBrainAccessor.setRamTicks(UniformIntProvider.create(60, 120));
     }
 }
